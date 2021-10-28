e8d32b9deb3cf749f8cb435b4828b2c418772197
Ian Ramos <ianramossantos@hotmail.com>
2021-10-28 10:52:20 -0300

    feat: create function for create a new todo

---
+++ src/functions/createTodo.ts
@@ -0,0 +1,32 @@
+import { v4 as uuidV4 } from 'uuid';
+
+import { APIGatewayProxyHandler } from 'aws-lambda';
+
+import { document } from '../utils/dynamodbClient';
+
+interface ICreateTodo {
+  title: string;
+  deadline: Date;
+}
+
+export const handle: APIGatewayProxyHandler = async event => {
+  const { userId } = event.pathParameters;
+
+  const { title, deadline } = JSON.parse(event.body) as ICreateTodo;
+
+  const todo_id = uuidV4();
+
+  await document.put({ 
+    TableName: 'users_todos', 
+    Item: { id: todo_id, user_id: userId, title, done: false, deadline: new Date(deadline) } 
+  }).promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({ 
+      message: 'Todo created successfully!',
+      todo: { id: todo_id, userId, title, done: false, deadline }
+    }),
+    headers: { 'Content-Type': 'application/json' }
+  }
+}
--- src/functions/hello.ts
+++
@@ -1,7 +0,0 @@
-export const handle = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hi challenge ignite serverless' }),
-    headers: { 'Content-Type': 'application/json' },
-  };
-};
